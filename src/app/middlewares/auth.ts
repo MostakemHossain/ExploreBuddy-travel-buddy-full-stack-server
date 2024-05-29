af95a0199832a281e4ac054687bde1694395c82d
MostakemHossain <mostakememon123@gmail.com>
2024-05-29 12:46:16 +0600

    role base access and appError implement

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import config from "../../config";
+import AppError from "../../error/AppError";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
+
+const auth = (...roles: string[]) => {
+  return async (
+    req: Request & { user?: any },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+      const verifyUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt_access_serect as string
+      );
+      console.log(verifyUser.role);
+      req.user = verifyUser;
+      if (roles.length && !roles.includes(verifyUser.role)) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default auth;
--- src/app/modules/trips/trip.routes.ts
+++ src/app/modules/trips/trip.routes.ts
@@ -1,8 +1,14 @@
+import { UserRole } from "@prisma/client";
 import express from "express";
+import auth from "../../middlewares/auth";
 import { tripController } from "./trip.controller";
 const router = express.Router();
 
-router.post("/trips", tripController.createTrip);
+router.post(
+  "/trips",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  tripController.createTrip
+);
 router.get("/trips", tripController.getAllTrip);
 
 export const tripRoutes = router;
