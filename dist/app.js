1e9ffcb51275cf1979e9038bc4b076e219c625db
MostakemHossain <mostakememon123@gmail.com>
2024-06-18 12:08:04 +0600

    handle env

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
@@ -12,16 +13,21 @@ const app = (0, express_1.default)();
 const allowedOrigins = [
     "https://explore-buddy-travel-buddy-full-stack-client.vercel.app", // Add other allowed origins here
 ];
-app.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (origin && allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+app.use((0, cors_1.default)({
+    origin: allowedOrigins, // Use allowedOrigins array
+    credentials: true,
+    methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"], // Include PATCH method
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "x-client-key",
+        "x-client-token",
+        "x-client-secret",
+        "Authorization",
+    ],
+}));
 app.use((0, cookie_parser_1.default)());
 // Parser
 app.use(express_1.default.json());
