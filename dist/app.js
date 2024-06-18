97f11169a1ddb884d026606e902f5a5dffd4c8c4
MostakemHossain <mostakememon123@gmail.com>
2024-06-19 00:40:22 +0600

    handle cors

--- dist/app.js
+++ dist/app.js
@@ -10,10 +10,7 @@ const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalEr
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    credentials: true,
-}));
+app.use((0, cors_1.default)());
 // Parser
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
