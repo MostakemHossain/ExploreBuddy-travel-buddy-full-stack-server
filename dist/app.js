8c33c155974afb359eaa793ea43ccb11a1df298e
MostakemHossain <mostakememon123@gmail.com>
2024-06-19 23:52:30 +0600

    frontend url link add in cors

--- dist/app.js
+++ dist/app.js
@@ -11,11 +11,12 @@ const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalEr
 const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["https://explore-buddy-travel-buddy-full-stack-client.vercel.app"],
+    origin: ["http://localhost:3000"],
     credentials: true,
 }));
 // Parser
 app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send({
