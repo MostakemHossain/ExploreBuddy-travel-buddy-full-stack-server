d75ee2c9058f8250a5eaab95fa3e1fc419aae114
MostakemHossain <mostakememon123@gmail.com>
2024-06-19 10:38:30 +0600

    handle cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 
 const app: Application = express();
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+const allowedOrigins = [
+  "https://explore-buddy-travel-buddy-full-stack-client.vercel.app", // Add other allowed origins here
+];
+app.use(function (req, res, next) {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 // Parser
 app.use(cookieParser());
 app.use(express.json());
