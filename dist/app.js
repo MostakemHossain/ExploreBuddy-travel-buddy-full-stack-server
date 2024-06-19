05b05843dbe653c416304cbb28ee30fd92519df7
MostakemHossain <mostakememon123@gmail.com>
2024-06-19 09:54:07 +0600

    some modification

--- dist/app.js
+++ dist/app.js
@@ -10,31 +10,7 @@ const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalEr
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
-const allowedOrigins = [
-    "https://explore-buddy-travel-buddy-full-stack-client.vercel.app",
-];
-app.use((0, cors_1.default)({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "x-client-key",
-        "x-client-token",
-        "x-client-secret",
-        "Authorization",
-    ],
-}));
+app.use((0, cors_1.default)());
 // Parser
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,22 @@ import router from "./app/routes";
 
 const app: Application = express();
 
-app.use(cors());
+// CORS configuration
+app.use(
+  cors({
+    origin: "https://explore-buddy-travel-buddy-full-stack-client.vercel.app", // allow this origin
+    methods: "GET, POST, PUT, DELETE, OPTIONS", // allowed methods
+    allowedHeaders: "Content-Type, Authorization", // allowed headers
+    credentials: true, // allow credentials such as cookies
+  })
+);
 
 // Parser
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
