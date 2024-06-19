52de38fa3f308c636bc45ca7ebd96ee3f999cedf
MostakemHossain <mostakememon123@gmail.com>
2024-06-19 20:50:51 +0600

    previous commit

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,43 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
 
 const app: Application = express();
 
-// CORS Middleware Configuration
 app.use(
   cors({
-    origin: "https://explore-buddy-travel-buddy-full-stack-client.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-      "x-client-key",
-      "x-client-token",
-      "x-client-secret",
-      "Authorization",
-    ],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 
-// Ensure CORS headers are set on all responses
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://explore-buddy-travel-buddy-full-stack-client.vercel.app"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
-
-// Parser
-app.use(cookieParser());
+// parser
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    message: "Explore Buddy API.............",
+    message: "Explore buddy client",
   });
 });
 
-// Global error handler
+app.use("/api", router);
+
 app.use(globalErrorHandler);
 
-// Not found routes
-app.use(notFound);
-
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found",
+    },
+  });
 });
 
 export default app;
